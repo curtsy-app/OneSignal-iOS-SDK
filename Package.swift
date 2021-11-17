2ac150b0a9fe7f1b6d6a5e76c42f521386d078e6
Jon Willis <jondwillis@gmail.com>
2021-11-17 16:30:33 -0500

    OneSignal (as xcframework binaryTarget)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,12 +11,10 @@ let package = Package(
             targets: ["OneSignal"]),
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "OneSignal",
-            dependencies: [],
-            path: "iOS_SDK/OneSignalSDK/",
-            sources: ["Source"],
-            publicHeadersPath:"SwiftPM/Public/Headers"),
+            url: "https://github.com/OneSignal/OneSignal-iOS-SDK/releases/download/3.9.1/OneSignal.xcframework.zip",
+            checksum: "d6a254e033f98e703820247df8d869e8cbe4bd3a6fcb590892dc9a8283072108"),
     ]
 )
 
